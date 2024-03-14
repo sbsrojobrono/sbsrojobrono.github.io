8d048c99d241607779e66e0ac261978a50ffd601
sbsrojobrono <163375456+sbsrojobrono@users.noreply.github.com>
2024-03-14 04:29:36 +0000

    delete

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <NuxtLayout></NuxtLayout>
-  </div>
-</template>
--- pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>Hello word</h1>
-</template>
-<script setup>
-    definePageMeta({
-        layout: 'master'
-    })
-</script>
